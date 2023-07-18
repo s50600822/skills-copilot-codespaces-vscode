5a73ee1b3862971e4b69e98788ee4a4bf66c47ee
HOA PHAN <s50600822@gmail.com>
2023-07-18 01:58:59 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// create web server with express
+const express = require('express');
+const app = express();
+// create a port
+const port = 3000;
+// create a path
+const path = require('path');
+// create a body parser
+const bodyParser = require('body-parser');      
+// create a mongoose    
+const mongoose = require('mongoose');
+// create a method override
+const methodOverride = require('method-override');
+// create a moment
+const moment = require('moment');

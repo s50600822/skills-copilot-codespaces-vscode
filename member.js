841c7ffd541170bc030c55b56d2e1e8e8f2018e5
HOA PHAN <s50600822@gmail.com>
2023-07-18 01:50:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    var contactBtn = document.getElementById("contactBtn");
+    var aboutBtn = document.getElementById("aboutBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    memberBtn.style.backgroundColor = "#4CAF50";
+    skillsBtn.style.backgroundColor = "#333";
+    contactBtn.style.backgroundColor = "#333";
+    aboutBtn.style.backgroundColor = "#333";
+}git add member.js
